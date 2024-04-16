fae19616b90b1cbeb04a049cdb1bbcc689127c99
19170960 <145975590+19170960@users.noreply.github.com>
2024-04-16 21:37:15 +0100

    Week 9 (Backdated 16/04/2024)
    Week 9 (Backdated 16/04/2024)
    
    - RSA encryption algorithm has been implemented (as apart of SMS transmissions.)
    - The encryption algorithm has been configured to use 4096 bytes of data.
    - Twilio authentication token has been updated (as it expired.)

--- EncryptedCommunicationApplication/SMS.cs
+++ EncryptedCommunicationApplication/SMS.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -47,13 +48,21 @@ namespace EncryptedCommunicationApplication
         private void button1_Click(object sender, EventArgs e)
         // When button is clicked, execute below indented code
         {
-            TwilioClient.Init("AC21300f0aa6ad6af38121f633cf450cf6", "8744083f8c4c1e9a5722b99e1b4cac97");
+            TwilioClient.Init("AC21300f0aa6ad6af38121f633cf450cf6", "a717799949c8d665805043f86767ebd3");
             // Configure twilio client credentials
 
-            MessageResource SMStransmit = MessageResource.Create(to: new Twilio.Types.PhoneNumber(textBox2.Text), from: new Twilio.Types.PhoneNumber("+447700171424"), body: textBox1.Text);
-            // Transmit an SMS using a mobile phone number, and body (inputted using the provided text-boxes.) 
-            richTextBox1.AppendText("Me: " + textBox1.Text + "\n");
-            // Display transmitted SMS within the provided text-box.
+            RSACryptoServiceProvider encryptionRSA = new RSACryptoServiceProvider(4096);
+            // Configure RSA encryption algorithm to use 4096 bytes of data
+
+            string textCipher = Convert.ToBase64String(encryptionRSA.Encrypt(Encoding.UTF8.GetBytes(textBox1.Text), false));
+            // Encrypt plain text message body
+
+            string textDecrypted = Encoding.UTF8.GetString(encryptionRSA.Decrypt(Convert.FromBase64String(textCipher), false));
+            // Decrypt plain text message body
+            MessageResource SMStransmit = MessageResource.Create(to: new Twilio.Types.PhoneNumber(textBox2.Text), from: new Twilio.Types.PhoneNumber("+447700171424"), body: textDecrypted);
+            // Transmit an SMS using a mobile phone number, and decrypted body (inputted using the provided text-boxes.) 
+            richTextBox1.AppendText("Me: " + textDecrypted + "\n");
+            // Display the transmitted SMS (in plain-text) within the provided text-box.
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
