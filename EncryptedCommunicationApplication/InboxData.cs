f54430279a4ff68d3b5fa22a7455ec3dd02a66eb
19170960 <145975590+19170960@users.noreply.github.com>
2024-04-16 19:35:15 +0100

    Week 6 (Backdated 16/04/2024)
    Week 6 (Backdated 16/04/2024)
    
    - An executable Windows form application has been developed.
    - This application supports a GUI (containing elements that can be used for later implementations e.g. SMS communication.)

---
+++ EncryptedCommunicationApplication/InboxData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptedCommunicationApplication
+{
+    public partial class InboxData : Form
+    {
+        public InboxData()
+        {
+            InitializeComponent();
+        }
+    }
+}
