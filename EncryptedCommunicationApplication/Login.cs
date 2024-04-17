dde0a8f6cfa42bde7603359a6f26a68a802c88b6
19170960 <145975590+19170960@users.noreply.github.com>
2024-04-17 17:14:29 +0100

    Week 19 (Backdated 17/04/2024)
    Week 19 (Backdated 17/04/2024)
    
    - Login authentication process has been implemented. Users can login using tools provided by the GUI interface. Inputted username/password information is verified against username/password data (stored within the database.) If an account with the inputted information exists, then access will be granted. If an account doesn't exist (with the inputted information) then access is not granted.

--- EncryptedCommunicationApplication/Login.cs
+++ EncryptedCommunicationApplication/Login.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,8 +19,32 @@ namespace EncryptedCommunicationApplication
         }
 
         private void button2_Click(object sender, EventArgs e)
+        // If login button was clicked, execute below indented code
         {
+            MySqlConnection connectionSQL = new MySqlConnection("server=127.0.0.1;database=databaseapplication;uid=root;password=backdoor");
+            // Configure SQL connection 
+            connectionSQL.Open();
+            // Open connection to configured database
 
+            string stringQueryUserID = "SELECT userID FROM user WHERE username = @username and password = @password";
+            // String query that selects user ID from users with username/password that matches input
+
+            MySqlCommand querySQLUserID = new MySqlCommand(stringQueryUserID, connectionSQL);
+            // Create query using string
+
+            querySQLUserID.Parameters.AddWithValue("@username", richTextBox1.Text);
+            querySQLUserID.Parameters.AddWithValue("@password", richTextBox2.Text);
+            // Configure username and password inputs for query (username, and password)
+
+            if(querySQLUserID.ExecuteScalar() != null)
+            // If a user exists with the username, and password inputted (within the user table, located in the database) then execute the below indented code
+            {
+                Home home = new Home();
+                this.Hide();
+                // Close current window
+                home.Show();
+                // Display home window
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -30,6 +55,16 @@ namespace EncryptedCommunicationApplication
             // Close current window
             start.Show();
             // Display start window
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
